61c7d3a27d24cb4aef84615bdc5d0cb6c60720b9
Gakuto Tobinaga <g-tobinaga@fabrica-com.co.jp>
2025-01-07 07:48:19 +0900

    fix: fixed Message

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "シカタス",
+  description: "あなたの人生を資格で彩ろう。",
 };
 
 const geistSans = Geist({
